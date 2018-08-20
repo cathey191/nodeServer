942035c03dbe6a2d303330c3239140c1c69394d6
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-20 21:55:09 +0000

    added first routes section for our nodejs server

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const http = require('http');
 
 var server = http.createServer(function(request, response) {
-	response.writeHead(200, { 'Content-Type': 'text/plain' });
-	response.end('This is running from a server');
+	var page;
+	if (request.url === '/') {
+		page = 'home';
+	} else if (request.url === '/contact') {
+		page = 'contact';
+	} else if (request.url === '/about') {
+		page = 'about';
+	} else {
+		page = '404 page not found';
+	}
+
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.end(`
+		<html>
+			<head>
+				<title>Node Server</title>
+			</head>
+			<body>
+				<h1>${page}</h1>
+				<p>${request.url}</p>
+				<p>${request.method}</p>
+			</body>
+		</html>
+		`);
 });
 server.listen(3000);
