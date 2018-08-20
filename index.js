2e39bf120d69cd06e527e1d7f27427faedd0bb39
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-20 23:50:22 +0000

    linked public files to server

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 var server = http.createServer(function(request, response) {
-	var page;
-	if (request.url === '/') {
-		page = 'home';
-	} else if (request.url === '/contact') {
-		page = 'contact';
+	console.log(`${request.method} request for ${request.url}`);
+	if (request.url === '/' || request.url === '/index') {
+		fs.readFile('./public/index.html', 'UTF-8', function(error, contents) {
+			if (error) {
+				console.log('Error, something went wrong');
+				console.log(error);
+			} else {
+				response.writeHead(200, { 'Content-Type': 'text/html' });
+				response.end(contents);
+			}
+		});
 	} else if (request.url === '/about') {
-		page = 'about';
-	} else {
-		page = '404 page not found';
+		fs.readFile('./public/about.html', 'UTF-8', function(error, contents) {
+			if (error) {
+				console.log('Error, something went wrong');
+				console.log(error);
+			} else {
+				response.writeHead(200, { 'Content-Type': 'text/html' });
+				response.end(contents);
+			}
+		});
+	} else if (request.url === '/contact') {
+		fs.readFile('./public/contact.html', 'UTF-8', function(error, contents) {
+			if (error) {
+				console.log('Error, something went wrong');
+				console.log(error);
+			} else {
+				response.writeHead(200, { 'Content-Type': 'text/html' });
+				response.end(contents);
+			}
+		});
+	} else if (request.url === '/extras') {
+		fs.readFile('./public/extras.html', 'UTF-8', function(error, contents) {
+			if (error) {
+				console.log('Error, something went wrong');
+				console.log(error);
+			} else {
+				response.writeHead(200, { 'Content-Type': 'text/html' });
+				response.end(contents);
+			}
+		});
+	} else if (request.url.match(/.css$/)) {
+		var cssPath = path.join(__dirname, 'public', request.url);
+		var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+		response.writeHead(200, { 'Content-Type': 'text/css' });
+		fileStream.pipe(response);
+		// } else if (request.url.match(/.png$/)) {
+		// 	var cssPath = path.join(__dirname, 'public', request.url);
+		// 	var fileStream = fs.readFileSync(cssPath, 'UTF-8');
+		// 	response.writeHead(200, { 'Content-Type': 'image/png' });
+		// 	response.end(response);
+		// } else if (request.url.match(/.jpg$/)) {
+		// 	var cssPath = path.join(__dirname, 'public', request.url);
+		// 	var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+		// 	response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+		// 	fileStream.pipe(response);
+	} else if (request.url.match(/.js$/)) {
+		var cssPath = path.join(__dirname, 'public', request.url);
+		var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+		response.writeHead(200, { 'Content-Type': 'text/js' });
+		fileStream.pipe(response);
 	}
-
-	response.writeHead(200, { 'Content-Type': 'text/html' });
-	response.end(`
-		<html>
-			<head>
-				<title>Node Server</title>
-			</head>
-			<body>
-				<h1>${page}</h1>
-				<p>${request.url}</p>
-				<p>${request.method}</p>
-			</body>
-		</html>
-		`);
 });
 server.listen(3000);
