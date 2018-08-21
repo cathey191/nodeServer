2ecdc939e38be51379c99a2adc553ceb05fdf1b4
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2018-08-21 21:47:37 +0000

    local data, sorting

--- index.js
+++ index.js
@@ -1,69 +1,101 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const data = require('./data/products');
 
 var server = http.createServer(function(request, response) {
 	console.log(`${request.method} request for ${request.url}`);
-	if (request.url === '/' || request.url === '/index') {
-		fs.readFile('./public/index.html', 'UTF-8', function(error, contents) {
-			if (error) {
-				console.log('Error, something went wrong');
-				console.log(error);
-			} else {
-				response.writeHead(200, { 'Content-Type': 'text/html' });
-				response.end(contents);
-			}
-		});
-	} else if (request.url === '/about') {
-		fs.readFile('./public/about.html', 'UTF-8', function(error, contents) {
-			if (error) {
-				console.log('Error, something went wrong');
-				console.log(error);
-			} else {
-				response.writeHead(200, { 'Content-Type': 'text/html' });
-				response.end(contents);
-			}
-		});
-	} else if (request.url === '/contact') {
-		fs.readFile('./public/contact.html', 'UTF-8', function(error, contents) {
-			if (error) {
-				console.log('Error, something went wrong');
-				console.log(error);
-			} else {
-				response.writeHead(200, { 'Content-Type': 'text/html' });
-				response.end(contents);
-			}
-		});
-	} else if (request.url === '/extras') {
-		fs.readFile('./public/extras.html', 'UTF-8', function(error, contents) {
-			if (error) {
-				console.log('Error, something went wrong');
-				console.log(error);
-			} else {
-				response.writeHead(200, { 'Content-Type': 'text/html' });
-				response.end(contents);
-			}
-		});
-	} else if (request.url.match(/.css$/)) {
-		var cssPath = path.join(__dirname, 'public', request.url);
-		var fileStream = fs.createReadStream(cssPath, 'UTF-8');
-		response.writeHead(200, { 'Content-Type': 'text/css' });
-		fileStream.pipe(response);
-	} else if (request.url.match(/.png$/)) {
-		var imagePath = path.join(__dirname, 'public', request.url);
-		var imageStream = fs.createReadStream(imagePath);
-		response.writeHead(200, { 'Content-Type': 'image/png' });
-		imageStream.pipe(response);
-	} else if (request.url.match(/.jpg$/)) {
-		var imagePath = path.join(__dirname, 'public', request.url);
-		var imageStream = fs.createReadStream(imagePath);
-		response.writeHead(200, { 'Content-Type': 'image/jpeg' });
-		imageStream.pipe(response);
-	} else if (request.url.match(/.js$/)) {
-		var cssPath = path.join(__dirname, 'public', request.url);
-		var fileStream = fs.createReadStream(cssPath, 'UTF-8');
-		response.writeHead(200, { 'Content-Type': 'text/js' });
-		fileStream.pipe(response);
+
+	// GET requests
+	if (request.method === 'GET') {
+		// HTML pages
+		if (request.url === '/' || request.url === '/index') {
+			fs.readFile('./public/index.html', 'UTF-8', function(error, contents) {
+				if (error) {
+					console.log('Error, something went wrong');
+					console.log(error);
+				} else {
+					response.writeHead(200, { 'Content-Type': 'text/html' });
+					response.end(contents);
+				}
+			});
+		} else if (request.url === '/about') {
+			fs.readFile('./public/about.html', 'UTF-8', function(error, contents) {
+				if (error) {
+					console.log('Error, something went wrong');
+					console.log(error);
+				} else {
+					response.writeHead(200, { 'Content-Type': 'text/html' });
+					response.end(contents);
+				}
+			});
+		} else if (request.url === '/contact') {
+			fs.readFile('./public/contact.html', 'UTF-8', function(error, contents) {
+				if (error) {
+					console.log('Error, something went wrong');
+					console.log(error);
+				} else {
+					response.writeHead(200, { 'Content-Type': 'text/html' });
+					response.end(contents);
+				}
+			});
+		} else if (request.url === '/extras') {
+			fs.readFile('./public/extras.html', 'UTF-8', function(error, contents) {
+				if (error) {
+					console.log('Error, something went wrong');
+					console.log(error);
+				} else {
+					response.writeHead(200, { 'Content-Type': 'text/html' });
+					response.end(contents);
+				}
+			});
+			// CSS link
+		} else if (request.url.match(/.css$/)) {
+			var cssPath = path.join(__dirname, 'public', request.url);
+			var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+			response.writeHead(200, { 'Content-Type': 'text/css' });
+			fileStream.pipe(response);
+			// JS link
+		} else if (request.url.match(/.js$/)) {
+			var cssPath = path.join(__dirname, 'public', request.url);
+			var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+			response.writeHead(200, { 'Content-Type': 'text/js' });
+			fileStream.pipe(response);
+			// PNG link
+		} else if (request.url.match(/.png$/)) {
+			var imagePath = path.join(__dirname, 'public', request.url);
+			var imageStream = fs.createReadStream(imagePath);
+			response.writeHead(200, { 'Content-Type': 'image/png' });
+			imageStream.pipe(response);
+			// JPG link
+		} else if (request.url.match(/.jpg$/)) {
+			var imagePath = path.join(__dirname, 'public', request.url);
+			var imageStream = fs.createReadStream(imagePath);
+			response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+			imageStream.pipe(response);
+			// get local data
+		} else if (request.url === '/allProducts') {
+			response.writeHead(200, { 'Content-Type': 'text/json' });
+			response.end(JSON.stringify(data));
+			// checking data for products in stock
+		} else if (request.url === '/inStock') {
+			inStock(response);
+		}
+
+		// forms POST
+	} else if (request.method === 'POST') {
+		if (request.url === '/formSubmit') {
+			console.log(request);
+			response.writeHead(200, { 'Content-Type': 'text/plain' });
+			response.end('Thank you for her new name');
+		}
 	}
 });
 server.listen(3000);
+
+function inStock(response) {
+	var stock = data.filter(function(item) {
+		return item.inStock === true;
+	});
+	response.end(JSON.stringify(stock));
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,14 @@
 console.log('js is running');
+
+$.ajax({
+	url: 'http://192.168.33.10:3000/allProducts',
+	dataType: 'json',
+	type: 'GET',
+	success: function(data) {
+		console.log(data);
+	},
+	error: function(error) {
+		console.log('Error getting products');
+		console.log(error);
+	}
+});
